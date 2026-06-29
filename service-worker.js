b045c9722be04afb3125ca3c8b9e37d11e4c22e8
mfredagsvik <m.fredagsvik@gmail.com>
2026-06-29 12:26:14 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='dart-score-v14-fresh-player-roast-db-1';
+const CACHE='dart-score-v14-1-random-start-clean-records-1';
 const ASSETS=['./','./index.html','./app.js','./roasts.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
