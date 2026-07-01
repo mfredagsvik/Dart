c326e09ee0cf4b8f036fa1820d61081d93beb832
mfredagsvik <m.fredagsvik@gmail.com>
2026-07-01 08:59:26 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='dart-score-v14-1-random-start-clean-records-1';
-const ASSETS=['./','./index.html','./app.js','./roasts.js','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='dart-score-v16-big-scoreboard-1';
+const ASSETS=['./','./index.html','./style.css','./app.js','./roasts.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
